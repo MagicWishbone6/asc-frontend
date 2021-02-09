03a9d0ecbdb0c715da000b9a14721730b9980798
Caitlin <caitlinlamprecht@outlook.com>
2021-02-09 12:48:08 -0600

    updated users component

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,36 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
-import Card from 'react-bootstrap/Card';
+import User from './User'
 
-function Users(props) {
+function Users() {
+
+    const [users, setUsers] = useState([])
+ 
+    const fetchUsers = () => {
+        const apiUrl = 'https://after-school-club.herokuapp.com/users';
+        fetch(apiUrl)
+          .then((response) => response.json())
+          .then((data) => setUsers(data)) 
+    }
+
+    useEffect(() => {
+        fetchUsers()
+    }, []) 
+
+
     return (
         <Container
         className="align-items-start">
             <h5>Users Component</h5>
-            <Card
-            className="align-items-start">
-                <Card.Header
-                className="d-inline-flex"
-                style={{
-                    width: '100%'
-                }}>
-                    <Card.Title
-                    className="mb-1">
-                        <a 
-                        href="/"
-                        style={{
-                            color: 'black'
-                        }}>
-                            User Username
-                        </a>
-                    </Card.Title>
-                </Card.Header>
-                <Card.Body>
-                    <Card.Text>
-                        Some Information relating to User
-                    </Card.Text>
-                </Card.Body>
-            </Card>
+            {users.map(user => 
+                <User
+                username={user.username} 
+                firstName={user.firstName}
+                lastName={user.lastName}
+                email={user.email}
+                status={user.status}
+                bio={user.bio}
+                password={user.password}
+                />
+            )}
         </Container>
     );
 }
